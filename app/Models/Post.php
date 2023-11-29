bc45643627cc2784aa3121496b58d00919cd4b09
fairuzulum <usleunmdi32@gmail.com>
2023-11-29 11:40:57 +0700

    eloquent accessor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -14,4 +15,11 @@ class Post extends Model
         'title',
         'content',
     ];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => asset('/storage/posts/' . $image)
+        );
+    }
 }
