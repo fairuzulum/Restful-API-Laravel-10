025e06247eb963fd6f2b51c4bc7d7009954c1383
fairuzulum <usleunmdi32@gmail.com>
2023-12-26 21:02:08 +0700

    update insert data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -14,4 +16,34 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
         return new PostResource(true, 'List Data Posts', $posts);
     }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,7 +19,7 @@ class Post extends Model
     protected function image(): Attribute
     {
         return Attribute::make(
-            get: fn($image) => asset('/storage/posts/' . $image)
+            get: fn ($image) => asset('/storage/posts/' . $image)
         );
     }
 }
